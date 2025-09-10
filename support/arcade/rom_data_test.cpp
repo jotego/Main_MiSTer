b476056ca720e9c40b2df5f6e7adfc6b1ef3615d
Jose Tejada <jose.tejada@jotego.es>
2025-09-10 22:30:51 +0200

    wip

--- support/arcade/rom_data_test.cpp
+++ support/arcade/rom_data_test.cpp
@@ -52,17 +52,45 @@ int rom_data(const uint8_t *buf, int chunk, int map)
 	return 1;
 }
 
-void print_merged() {
+void print_merged(const char *expected) {
+	for(int i=0; i<8; i++) printf("%X",romdata[i]);
+	printf(" - got\n");
+	printf("%s - expected\n", expected);
+}
+
+void test_map(int map, const char *expected ) {
+	uint8_t buf[8]={0,1,2,3,4,5,6,7};
 	for(int i=0; i<8; i++) {
-		printf("%X ",romdata[i]);
+		romdata[i]=0;
+		romlen[i]=0;
 	}
-	printf("- got\n");
-	printf("0 0 1 0 2 0 3 0 - expected\n");
+	rom_data(buf, 8, map);
+	for(int i=0; i<8; i++) {
+		if(expected[i]==0){
+			printf("Expected is too short!\n");
+			exit(1);
+		}
+		if(romdata[i]!=(uint8_t)(expected[i]-'0')) {
+			printf("map=%04X -> %X != %c at index %d\n",map,romdata[i],expected[i],i);
+			print_merged(expected);
+			exit(1);
+		}
+	}
 }
 
 int main() {
-	uint8_t buf[8]={0,1,2,3,4,5,6,7};
 	int map=0x0201;
-	rom_data(buf, 8, map);
-	print_merged();
+	test_map(0x0001,"00001000");
+	test_map(0x0021,"01002300");
+	test_map(0x4321,"01234567");
+	test_map(0x0012,"10003200");
+	test_map(0x1234,"32107654");
+	test_map(0x1200,"00100032");
+	test_map(0x2100,"00010023");
+	test_map(0x0201,"00102030");
+	// These will fail because the sequence cannot skip bytes
+	// test_map(0x0034,"32007600");
+	// test_map(0x0043,"23006700");
+	// test_map(0x4300,"00230067");
+	return 0;
 }
